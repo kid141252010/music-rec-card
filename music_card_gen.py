e1970fdda16c21c2140be8f443b4cdd1e997cb6c
kid141252010 <250306205+kid141252010@users.noreply.github.com>
2026-03-27 02:10:51 +0800

    Update music_card_gen.py

--- music_card_gen.py
+++ music_card_gen.py
@@ -16,7 +16,7 @@ import emoji
 
 from ttml.ttml import TTML
 
-TTML_DB_URL_PREFIX = "https://raw.githubusercontent.com/kid141252010/amll-ttml-db"
+TTML_DB_URL_PREFIX = "https://raw.githubusercontent.com/kid141252010/amll-ttml-db/refs/heads/main/"
 
 
 class MusicCard:
